1441f8662c25272d0ac65a941387fc90fed1cb9d
zjooy <zjoy_@outlook.com>
2023-04-01 11:35:21 -0300

    Home

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color){
+    const nav = document.querySelector('.nav');
+    nav.style.backgroundColor = color;
+}
